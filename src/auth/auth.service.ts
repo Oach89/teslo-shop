0828c3f4030dc263a82b09467bbd77afdfa1f931
Oscar Cach <oscarcach49@gmail.com>
2023-05-29 07:50:10 -0500

    JwtStrategy

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,65 @@
+import {
+	BadRequestException,
+	Injectable,
+	InternalServerErrorException,
+	UnauthorizedException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { LoginUserDto } from './dto/login-user.dto';
+
+@Injectable()
+export class AuthService {
+	constructor(
+		@InjectRepository(User)
+		private readonly userRepository: Repository<User>,
+	) {}
+	async create(createUserDto: CreateUserDto) {
+		try {
+			const { password, ...userData } = createUserDto;
+			const newUser = this.userRepository.create({
+				...userData,
+				password: bcrypt.hashSync(password, 10),
+			});
+			await this.userRepository.save(newUser);
+			delete newUser.password;
+			return newUser;
+			// Todo: retorno de JWT de acceso
+		} catch (error) {
+			this.handleDBErrors(error);
+		}
+	}
+
+	async login(loginUserDto: LoginUserDto) {
+		try {
+			const { email, password } = loginUserDto;
+			const user = await this.userRepository.findOne({
+				where: {
+					email,
+				},
+				select: {
+					email: true,
+					password: true,
+				},
+			});
+			if (!user)
+				throw new UnauthorizedException('Credentials are not valid(email)');
+			if (!bcrypt.compareSync(password, user.password))
+				throw new UnauthorizedException('Credentials are not valid(password)');
+			return user;
+		} catch (error) {
+			this.handleDBErrors(error);
+		}
+	}
+
+	private handleDBErrors(error: any): never {
+		if (error.code === '23505') {
+			throw new BadRequestException(error.detail);
+		}
+		console.log(error);
+		throw new InternalServerErrorException('Please check server logs');
+	}
+}
